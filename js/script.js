f7541ff51e4e2e91948797d3afd793d091fea51c
Pablq <pabloignaciophilipps@gmail.com>
2015-07-24 09:36:43 -0500

    to merge.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-alert("Track and field were all the information comes from.");
 
 
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $("p").hide();
-//     });
-// });
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+});
 
 
 /*
